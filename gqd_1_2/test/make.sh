a24e7fb63babc5c321ab08b8fb7a573371979287
Mian Lu <lumianph@gmail.com>
2015-12-06 16:16:45 +0800

    make.sh

--- gqd_1_2/test/make.sh
+++ gqd_1_2/test/make.sh
@@ -1,8 +1,8 @@
-GPU_ARCH=sm_13
-CUDA_SDK_HOME=/home/mian/NVIDIA_GPU_Computing_SDK
+CUDA_SDK_HOME="/home/mian/NVIDIA_GPU_Computing_SDK"
+NVCC_FLAGS="-m64 -gencode arch=compute_35,code=sm_35"
 
 echo "Compiling GPU kernel ......"
-nvcc gqdtest_kernel.cu -c -O3 -I../inc -I ../map -I$CUDA_SDK_HOME/C/common/inc -arch=$GPU_ARCH --opencc-options -OPT:Olimit=0
+nvcc gqdtest_kernel.cu -c -O3 -I../inc -I ../map -I$CUDA_SDK_HOME/C/common/inc $NVCC_FLAGS
 echo "Compiling test cases ......"
 g++ test_util.cpp -c -O3 -I ../inc -I /usr/local/cuda/include -fopenmp
 g++ benchmark.cpp -c -O3 -I../inc -I/usr/local/cuda/include -fopenmp
